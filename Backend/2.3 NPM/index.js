a86a3f151541d2b539ca1b78b6ebad5eec736fae
Namanagra <96942264+Namanagra@users.noreply.github.com>
2024-11-23 20:50:29 +0530

    WebPortfolio

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+// console.log(sillyName);
+// console.log('My name is ' + sillyName + '.');
+
+import {randomSuperhero} from "superheroes";
+
+const name = randomSuperhero();
+console.log(`I am ${name}!`);
